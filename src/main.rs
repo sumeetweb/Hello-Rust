01c2127f26b99cfb50771e63b464193c57100413
Sumeet Naik <sumeetweb@users.noreply.github.com>
2022-07-06 12:55:53 +0530

    Types, Generics and Traits

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use crate::step1::BalanceModule;
 mod step1;
+mod step2;
+mod step3;
 
 fn main() {
     // let mut balances:BalanceModule = step1::BalanceModule {
@@ -10,14 +11,44 @@ fn main() {
     
     // println!("{:?}", balances.balance.get(&1).unwrap());
 
-    let mut balances = BalanceModule::new();
-    balances.balance.insert(1, 100);
-    println!("{:?}", balances.balance.get(&1).unwrap());
+    // let mut balances = step1::BalanceModule::new();
+    // balances.balance.insert(1, 100);
+    // println!("{:?}", balances.balance.get(&1).unwrap());
 }
 
 #[test]
 pub fn test_step1(){
-    let mut balances = BalanceModule::new();
+    let mut balances = step1::BalanceModule::new();
+    balances.set_balance(1, 100);
+    balances.set_balance(2, 200);
+
+    assert!(balances.get_balance(1) == 100);
+    assert!(balances.get_balance(2) == 200);
+
+    balances.transfer(1, 2, 50);
+    assert!(balances.get_balance(2) == 250);
+}
+
+#[test]
+pub fn test_step2(){
+    let mut balances = step2::BalanceModule::new();
+    balances.set_balance(1, 100);
+    balances.set_balance(2, 200);
+
+    assert!(balances.get_balance(1) == 100);
+    assert!(balances.get_balance(2) == 200);
+
+    balances.transfer(1, 2, 50);
+    assert!(balances.get_balance(2) == 250);
+}
+
+#[test]
+pub fn test_step3(){
+
+    type AccountId = u32;
+    type Balance = u32;
+
+    let mut balances = step3::BalanceModule::new();
     balances.set_balance(1, 100);
     balances.set_balance(2, 200);
 
